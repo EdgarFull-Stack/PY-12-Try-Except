2b21865d11944faa6fb415fd7dcf38c968cee7d3
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-30 19:34:10 +0200

    2 Task

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ except Exception as e:
 print("Programa tęsia darbą")
 print('----------------------------------------------')
 #  Example
+ivestis1 = input("Įveskite sveiką skaičių")
+ivestis2 = input("Įveskite daliklį")
+
+try:
+    skaicius = int(ivestis1)
+    daliklis = int(ivestis2)
+    res = skaicius / daliklis
+    print(f'Rezultatas: {res}')
+except ValueError:
+    print("Mes crashinom su ValueError")
+    print("Paleiskite programą išnaujo ir ivestis padarykit kad tai būtų sveikas skaičius")
+except ZeroDivisionError:
+    print("Mes crashinom su ZeroDivisionError")
+    print("Pakeiskite daliklį iš 0 į kitą")
+
+print("Programa tęsia darbą")
--- tasks.py
+++ tasks.py
@@ -1,9 +1,27 @@
 # Task 1
-def dalinti(a,b):
+def dalinti(a:(int, float),b:(int, float)) -> float:
     try:
         return a / b
     except ZeroDivisionError:
         return 'Dalyba iš nulio negalima.'
+
 print(dalinti(10,2))
 print(dalinti(5,0))
 print(dalinti(8,4))
+print('----------------------------------------------')
+# Task 2
+input1 = input('Put integer: ')
+input2 = input('Put second integer: ')
+
+try:
+    first_numb = int(input1)
+    second_numb = int(input2)
+    res = first_numb / second_numb
+    print(f'Your result is: {res}')
+except ZeroDivisionError:
+    print('Change 0 to another integer')
+except ValueError:
+    print('Use numbers')
+print('----------------------------------------------')
+
+
